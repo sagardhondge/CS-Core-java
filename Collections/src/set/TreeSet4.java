787b37daec0b13344d77c5b4c8db6897822ebd74
sagardhondge <sagardhondge56@gmail.com>
2026-02-13 20:58:33 +0530

    Examples for TreeSet

--- Collections/src/set/TreeSet4.java
+++ Collections/src/set/TreeSet4.java
@@ -1,4 +1,17 @@
 package set;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class TreeSet4 {
+    public static void main(String[] args) {
+        TreeSet ts1 = new TreeSet();
+        ts1.add(100); ts1.add(200); ts1.add(300); ts1.add(300);
+        System.out.println(ts1);
+        SortedSet ss1= ts1.headSet(300);
+        SortedSet ss2= ts1.tailSet(300);
+        System.out.println(ss1);
+        System.out.println(ss2);
+
+    }
 }
--- Collections/src/set/TreeSet5.java
+++ Collections/src/set/TreeSet5.java
@@ -1,4 +1,29 @@
 package set;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class TreeSet5 {
+
+    public static void main(String[] args) {
+
+        TreeSet ts1 = new TreeSet();
+
+        ts1.add(200);
+        ts1.add(100);
+        ts1.add(300);
+        ts1.add(500);
+        ts1.add(400);
+
+        System.out.println(ts1);
+
+        SortedSet ss1 = ts1.headSet(300);
+        SortedSet ss2 = ts1.tailSet(300);
+
+        TreeSet ts2 = new TreeSet(ss1);
+        System.out.println(ts2);
+
+        TreeSet ts3 = new TreeSet(ss2);
+        System.out.println(ts3);
+    }
 }
