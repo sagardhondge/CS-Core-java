994358f8eda60f85efed705a90392d9b62e677ba
sagardhondge <sagardhondge56@gmail.com>
2026-02-16 16:01:25 +0530

    Examples on Collections

---
+++ Collections/src/collection/map/LinkedHashMap1.java
@@ -0,0 +1,21 @@
+package collection.map;
+
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMap1 {
+    public static void main(String[] args) {
+        LinkedHashMap lhm1 = new LinkedHashMap();
+        lhm1.put(2, 'y');
+        lhm1.put(1, 'x');
+        lhm1.put(3, 'z');
+        System.out.println(lhm1);
+        lhm1.put(10.5f, true);
+        lhm1.put("Cyber", 'a');
+        System.out.println(lhm1);
+        lhm1.put(null, 121);
+        System.out.println(lhm1);
+        lhm1.put(null, 121);
+        System.out.println(lhm1);
+    }
+}
