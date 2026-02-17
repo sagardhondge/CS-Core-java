6285a47b8c15fc83552a399d7d6f76b7e990213f
sagardhondge <sagardhondge56@gmail.com>
2026-02-17 15:42:36 +0530

    Examples on loops

---
+++ ControlFlowStatements/while4.java
@@ -0,0 +1,13 @@
+public class while4 {
+    public static void main(String[] args) {
+        int num=1;
+        while (num<=10)
+        {   if (num==3 || num==9)
+            {
+                System.out.println("Num is "+num);
+            }
+            num++;
+
+        }
+    }
+}
