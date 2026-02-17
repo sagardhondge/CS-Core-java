34324ecf53bd9db82005ef15c44d24cde5f36a7f
sagardhondge <sagardhondge56@gmail.com>
2026-02-17 11:58:12 +0530

    Examples on loops

---
+++ ControlFlowStatements/For7.java
@@ -0,0 +1,8 @@
+public class For7 {
+    public static void main(String[] args) {
+        for (int i = 0; i <5 ; System.out.println("one") ) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ ControlFlowStatements/for6.java
@@ -0,0 +1,7 @@
+public class for6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; ) {
+            System.out.println(i);
+        }
+    }
+}
