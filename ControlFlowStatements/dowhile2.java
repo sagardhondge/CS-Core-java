c5fa8ab8078358af93d1374145cb620d1ba7f0c9
sagardhondge <sagardhondge56@gmail.com>
2026-02-17 16:03:59 +0530

    Examples on loops

---
+++ ControlFlowStatements/dowhile2.java
@@ -0,0 +1,9 @@
+public class dowhile2 {
+    public static void main(String[] args) {
+        int i=5;
+        do {
+            System.out.println("the value of i is "+i);
+            i++;
+        }while (i<=6);
+    }
+}
---
+++ ControlFlowStatements/dowhile3.java
@@ -0,0 +1,10 @@
+public class dowhile3 {
+    public static void main(String[] args) {
+        int num =5;
+        do
+        {
+            System.out.println(num);
+            num--;
+        }while (num>=1);
+    }
+}
