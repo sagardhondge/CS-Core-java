13344611355e520c967d665f47a769c881ab306d
sagardhondge <sagardhondge56@gmail.com>
2026-02-20 15:43:25 +0530

    Examples on polymorphism

--- Polymorphism/Example24.java
+++ Polymorphism/Example24.java
@@ -1,8 +1,9 @@
 class Rbi
 {	void loans() 
 	{	System.out.println("ROI for Loans @RBI should be 8%");
-		final void processFess ()
-		System.out.println("Mandatory if 1% of amount");
+	}
+	void processFess()
+	{	System.out.println("Mandatory if 1% of amount");
 	}	
 }
 class Hdfc extends Rbi
@@ -22,9 +23,9 @@ class Example24
 {	public static void main(String[] args)
 	{	Rbi cust1 = new Hdfc();
 		cust1.loans();
-		Rbi cust1 = new Yes();
+		Rbi cust2 = new Yes();
 		cust2.loans();
-		Rbi cust3= new Icici()
+		Rbi cust3= new Icici();
 		cust3.processFess();
 	}
 }
