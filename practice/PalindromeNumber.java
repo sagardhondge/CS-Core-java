757ae7094b986ed5079125e76e14a53b3871cee7
sagardhondge <sagardhondge56@gmail.com>
2026-02-28 20:04:39 +0530

    practice  Examples

--- practice/PalindromeNumber.java
+++ practice/PalindromeNumber.java
@@ -1,3 +1,21 @@
 public class PalindromeNumber {
 
+    public static void main(String[] args) {
+
+        int number = 121;      // Change this value to test
+        int original = number;
+        int reverse = 0;
+
+        while (number > 0) {
+            int digit = number % 10;     // Extract last digit
+            reverse = reverse * 10 + digit;  // Build reversed number
+            number = number / 10;        // Remove last digit
+        }
+
+        if (original == reverse) {
+            System.out.println(original + " is a Palindrome");
+        } else {
+            System.out.println(original + " is Not a Palindrome");
+        }
+    }
 }
