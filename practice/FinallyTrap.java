07cbede78a31ef9586cf604f76ffb6334958a28c
sagardhondge <sagardhondge56@gmail.com>
2026-02-28 20:27:30 +0530

    Examples on practice

--- practice/FinallyTrap.java
+++ practice/FinallyTrap.java
@@ -1,2 +1,12 @@
 public class FinallyTrap {
+    static int test(){
+        try{
+            return 10;
+        }finally {
+            return 20; // it will overide the first return
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(test());
+    }
 }
